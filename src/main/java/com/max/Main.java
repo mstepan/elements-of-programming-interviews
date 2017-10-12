61df88e93e3032a159c256016a3e875544b8b5f5
Maksym Stepanenko <maksym.stepanenko@opower.com>
2017-10-12 15:55:20 +0300

    dependency plugin version updated

--- src/main/java/com/max/Main.java
+++ src/main/java/com/max/Main.java
@@ -1,21 +1,9 @@
 package com.max;
 
-import java.util.HashSet;
-import java.util.Set;
-
-
 public class Main {
 
     private Main() throws Exception {
 
-        Set<Integer> set1 = Set.of(1, 2, 3, 4, 5);
-        Set<Integer> set2 = Set.of(10, 20, 3, 5, 60);
-
-        Set<Integer> temp = new HashSet<>(set1);
-        temp.retainAll(set2);
-
-        Set<Integer> intersection = Set.of(temp.toArray(new Integer[0]));
-        System.out.println(intersection);
 
         System.out.println("java version: " + System.getProperty("java.version"));
     }
