fff7692033034f92f4a533cd77d1cdc062815b46
Maksym Stepanenko <maksym.stepanenko@opower.com>
2017-10-17 14:23:48 +0300

    A few simple plugin changes.

---
+++ src/main/java/com/max/Main.java
@@ -0,0 +1,15 @@
+package com.max;
+
+
+final class Main {
+
+
+    public static void main(String[] args) {
+
+        String str = "";
+
+        System.out.println("java version: " + System.getProperty("java.version"));
+    }
+
+
+}
