4b8d51c420087672129e803aacaf38ac6d12c788
Maksym Stepanenko <maksym.stepanenko@opower.com>
2017-11-22 11:02:56 +0200

    18.8. Compute the largest rectangle under the skyline fully completed.

--- src/main/java/com/max/epi/greedy/LargestRectangleUnderSkyline.java
+++ src/main/java/com/max/epi/greedy/LargestRectangleUnderSkyline.java
@@ -1,12 +1,84 @@
 package com.max.epi.greedy;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.base.Preconditions.checkNotNull;
+
 /**
- * 18.8. COMPUTE THE LARGEST RECTANGLE UNDER THE SKYLINE.
+ * 18.8. Compute the largest rectangle under the skyline.
  */
 final class LargestRectangleUnderSkyline {
 
     private LargestRectangleUnderSkyline() {
         throw new IllegalStateException("Can't instantiate utility only class");
+    }
+
+    /**
+     * time: O(N)
+     * space: O(N)
+     */
+    static int largestRectangle(int[] arr) {
+        checkNotNull(arr, "null 'arr' passed");
+        checkAllElementsPositive(arr);
+
+        if (arr.length == 0) {
+            return 0;
+        }
+
+        if (arr.length == 1) {
+            return arr[0];
+        }
+
+        int maxArea = 0;
+
+        Deque<ValueAndIndex> stack = new ArrayDeque<>();
+
+        for (int i = 0; i < arr.length + 1; ++i) {
+            final int val = (i == arr.length) ? 0 : arr[i];
+
+            int index = i;
+
+            while (!stack.isEmpty() && stack.peekFirst().value > val) {
+                ValueAndIndex last = stack.pop();
+
+                assert last.value > val;
+                assert last.index < i;
+
+                int lastArea = (i - last.index) * last.value;
+                assert lastArea > 0;
+
+                maxArea = Math.max(maxArea, lastArea);
+
+                index = last.index;
+            }
+
+            if (val != 0) {
+                stack.push(new ValueAndIndex(val, index));
+            }
+        }
+
+        assert maxArea > 0;
+        assert stack.isEmpty() : "not empty 'stack' detected at the end of algorithm";
+
+        return maxArea;
+    }
+
+    private static void checkAllElementsPositive(int[] arr) {
+        for (int val : arr) {
+            checkArgument(val > 0, "incorrect value %s, should be positive greater than 0", val);
+        }
+    }
+
+    private static class ValueAndIndex {
+        final int value;
+        final int index;
+
+        ValueAndIndex(int value, int index) {
+            this.value = value;
+            this.index = index;
+        }
     }
 
     /**
@@ -17,6 +89,7 @@ final class LargestRectangleUnderSkyline {
      * space: O(lgN)
      */
     static int largestRectangleDivideAndConquer(int[] arr) {
+        checkAllElementsPositive(arr);
         return largestRectangleRec(arr, 0, arr.length - 1);
     }
 
--- src/test/java/com/max/epi/greedy/LargestRectangleUnderSkylineTest.java
+++ src/test/java/com/max/epi/greedy/LargestRectangleUnderSkylineTest.java
@@ -1,10 +1,38 @@
 package com.max.epi.greedy;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import static org.junit.Assert.assertEquals;
 
 public final class LargestRectangleUnderSkylineTest {
+
+    @Rule
+    public final ExpectedException expectedEx = ExpectedException.none();
+
+    @Test
+    public void largestRectangle() {
+        int[] arr = {1, 4, 2, 5, 6, 3, 2, 6, 6, 5, 2, 1, 3};
+        assertEquals(20, LargestRectangleUnderSkyline.largestRectangle(arr));
+    }
+
+    @Test
+    public void largestRectangleNegativeValue() {
+        expectedEx.expect(IllegalArgumentException.class);
+        expectedEx.expectMessage("incorrect value -6, should be positive greater than 0");
+        LargestRectangleUnderSkyline.largestRectangle(new int[]{1, 4, 2, 5, 6, 3, 2, 6, -6, 5, 2, 1, 3});
+    }
+
+    @Test
+    public void largestRectangleOneElementArray() {
+        assertEquals(4, LargestRectangleUnderSkyline.largestRectangle(new int[]{4}));
+    }
+
+    @Test
+    public void largestRectangleEmptyArray() {
+        assertEquals(0, LargestRectangleUnderSkyline.largestRectangle(new int[0]));
+    }
 
     @Test
     public void largestRectangleDivideAndConquer() {
